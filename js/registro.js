0b792d93198ac6942bd0b3724c7baea1b6061fc1
Manuel Guijarro Sánchez <manu2506.1994@gmail.com>
2024-01-12 01:07:45 +0100

    update_server

--- js/registro.js
+++ js/registro.js
@@ -21,7 +21,7 @@ registroBtn.addEventListener('click',async  function(event)  {
     }
     try {
 
-        const response = await fetch(`http://127.0.0.1:5000/${valorRuta}/create-${valorRuta}`, {
+        const response = await fetch(`https://vecino-conecta-backend-jc8x51r7y-manuels-projects-84e6d30b.vercel.app/${valorRuta}/create-${valorRuta}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
